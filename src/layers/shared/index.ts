4d7385afaba72b909dc8a22f3bd2684752dd1279
2DC <kkts9308@gmail.com>
2024-04-05 23:33:56 +0900

    🎨 layers/shared에 index 추가 및 외부 공유 필요한 코드 public

---
+++ src/layers/shared/index.ts
@@ -0,0 +1,11 @@
+// lib
+export * from './lib/utils'
+
+// ui
+export * from './ui/separator'
+export * from './ui/button'
+export * from './ui/card'
+export * from './ui/container'
+
+// hooks
+export * from './hooks/use-typing-effect'
