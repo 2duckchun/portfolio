217418152fa9b6837ba835cbe8842533e6c6ee41
2DC <kkts9308@gmail.com>
2024-04-06 00:07:37 +0900

    ✨ useTypingEffect 기능 추가 및 shared index에 alert 컴포넌트 반영

--- src/layers/shared/index.ts
+++ src/layers/shared/index.ts
@@ -6,6 +6,7 @@ export * from './ui/separator'
 export * from './ui/button'
 export * from './ui/card'
 export * from './ui/section'
+export * from './ui/alert'
 
 // hooks
 export * from './hooks/use-typing-effect'
