1f860959a40805ea8905d642af09faac3217ac44
2duckchun <kkts9308@gmail.com>
2024-04-07 01:25:39 +0900

    🎨 inner-card 컴포넌트 개발 및 about-me-container에 반영

---
+++ src/layers/widgets/intro-block/inner-card.tsx
@@ -0,0 +1,43 @@
+import { cn } from '@/src/layers/shared/lib/utils'
+import { FunctionComponent, HTMLAttributes, ReactNode } from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/src/layers/shared'
+interface InnerCardProps extends HTMLAttributes<HTMLDivElement> {
+  title: string
+  description: string
+  children: ReactNode
+}
+
+const InnerCard: FunctionComponent<InnerCardProps> = ({
+  className,
+  title,
+  description,
+  children,
+  ...props
+}) => {
+  return (
+    <Card
+      className={cn(
+        'bg-white max-w-[1280px] min-h-[500px] m-auto rounded-3xl shadow-2xl p-3 lg:p-10',
+      )}
+      {...props}
+    >
+      <CardHeader>
+        <CardTitle className="text-center font-black-han-sans text-4xl">
+          {title}
+        </CardTitle>
+        <CardDescription className="text-center text-xl">
+          {description}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </Card>
+  )
+}
+
+export { InnerCard }
