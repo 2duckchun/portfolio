6eb1a506b2f2d7dc3daee6af4a2eace19e0c4368
2duckchun <kkts9308@gmail.com>
2024-04-08 19:51:11 +0900

    🎨 dialog ui 추가, useIsMounted 훅 생성, button variants에 tag 추가, shared 캡슐화 index 수정

--- src/layers/shared/index.ts
+++ src/layers/shared/index.ts
@@ -8,6 +8,11 @@ export * from './ui/card'
 export * from './ui/section'
 export * from './ui/alert'
 export * from './ui/inner-card'
+export * from './ui/dialog'
 
 // hooks
 export * from './hooks/use-typing-effect'
+export * from './hooks/use-is-mounted'
+
+// stores
+export * from './stores/use-modal-store'
