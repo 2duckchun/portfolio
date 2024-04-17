8c65bede6fb1df8cce54d0f06f4f6d1be890422c
2duckchun <kkts9308@gmail.com>
2024-04-17 18:42:32 +0900

    🎨 타이핑애니메이션 컨테이너 생성 후 page에서 분리

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,36 +6,17 @@ import {
   AlertTitle,
   InnerCard,
   Section,
-  useTypingEffect,
 } from '@/src/layers/shared'
-import { SkillSetContainer } from '@/src/layers/widgets'
+import { BgTypingContainer, SkillSetContainer } from '@/src/layers/widgets'
 import { AboutMeContainer } from '@/src/layers/widgets'
 import { ProjectRecordContainer } from '@/src/layers/widgets/intro-block/project-record-container'
 import Image from 'next/image'
 
 export default function Home() {
-  const { word } = useTypingEffect(
-    [
-      '협업하기 편한',
-      '끊임없이 발전하는',
-      '조직을 위해 일하는',
-      '기술적 호기심이 왕성한',
-    ],
-    130,
-    20,
-  )
   return (
     <main>
       <Section className="h-[300px] bg-[url('/images/cup-of-coffee.jpg')] bg-cover">
-        <div className="flex h-full grow items-center justify-center text-2xl md:text-3xl">
-          <div className="w-full text-center text-white">
-            <p className="h-[40px]">
-              <span className="font-semibold  text-blue-100">{word}</span>{' '}
-              개발자
-            </p>
-            <p>김태수의 포트폴리오입니다.</p>
-          </div>
-        </div>
+        <BgTypingContainer />
       </Section>
       <Section
         id="about-me"
---
+++ src/layers/widgets/intro-block/bg-typing-container.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent, HTMLAttributes } from 'react'
+import { useTypingEffect } from '../../shared'
+interface BgTypingContainerProps extends HTMLAttributes<HTMLDivElement> {}
+
+const BgTypingContainer: FunctionComponent<BgTypingContainerProps> = ({
+  className,
+  ...props
+}) => {
+  const { word } = useTypingEffect(
+    [
+      '협업하기 편한',
+      '끊임없이 발전하는',
+      '조직을 위해 일하는',
+      '기술적 호기심이 왕성한',
+    ],
+    130,
+    20,
+  )
+
+  return (
+    <div className="flex h-full grow items-center justify-center text-2xl md:text-3xl">
+      <div className="w-full text-center text-white">
+        <p className="h-[40px]">
+          <span className="font-semibold  text-blue-100">{word}</span> 개발자
+        </p>
+        <p>김태수의 포트폴리오입니다.</p>
+      </div>
+    </div>
+  )
+}
+
+export { BgTypingContainer }
