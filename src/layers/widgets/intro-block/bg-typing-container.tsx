28f8b34d528300bbca52b0360f563211ac55c4a7
2DC <kkts9308@gmail.com>
2024-10-18 22:21:56 +0900

    text: update text of bg-typing-container (top component)

--- src/layers/widgets/intro-block/bg-typing-container.tsx
+++ src/layers/widgets/intro-block/bg-typing-container.tsx
@@ -6,28 +6,33 @@ const BgTypingContainer: FunctionComponent<BgTypingContainerProps> = ({
   className,
   ...props
 }) => {
+  return (
+    <section className="h-[300px] bg-[url('/images/cup-of-coffee.jpg')] bg-cover">
+      <div className="flex h-full grow items-center justify-center text-2xl md:text-3xl">
+        <TypingAminationArea />
+      </div>
+    </section>
+  )
+}
+
+const TypingAminationArea = () => {
   const { word } = useTypingEffect(
     [
+      '코드를 깔끔하게 짜는',
       '협업하기 편한',
       '끊임없이 발전하는',
-      '조직을 위해 일하는',
-      '기술적 호기심이 왕성한'
+      '조직을 위해 일하는'
     ],
     130,
     20
   )
-
   return (
-    <section className="h-[300px] bg-[url('/images/cup-of-coffee.jpg')] bg-cover">
-      <div className="flex h-full grow items-center justify-center text-2xl md:text-3xl">
-        <div className="w-full text-center text-white">
-          <p className="h-[40px]">
-            <span className="font-semibold  text-blue-100">{word}</span> 개발자
-          </p>
-          <p>김태수의 포트폴리오입니다.</p>
-        </div>
-      </div>
-    </section>
+    <div className="w-full text-center text-white">
+      <p className="h-[40px]">
+        <span className="font-semibold  text-blue-100">{word}</span> 개발자
+      </p>
+      <p>김태수의 포트폴리오입니다.</p>
+    </div>
   )
 }
 
